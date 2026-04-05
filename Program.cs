3e316b73ee0e75656c8ceeea7a6b96df43b9feea
kuzhano <sw30243@gmail.com>
2026-04-06 00:42:49 +0700

    menambahkan class SayaMusicTrack

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        SayaMusicTrack laguFavorit = new SayaMusicTrack("Machine Love");
+
+        laguFavorit.PrintTrackDetails();
+        laguFavorit.IncreasePlayCount(163);
+        laguFavorit.PrintTrackDetails();
+
+    }
+}
---
+++ SayaMusicTrack.cs
@@ -0,0 +1,34 @@
+﻿class SayaMusicTrack
+{
+    private int id;
+    private int playCount;
+    private string title;
+
+    public SayaMusicTrack(string title)
+    {
+        Random random = new Random();
+        this.id = random.Next(1000, 10000);
+        this.playCount = 0;
+        this.title = title;
+    }
+
+    public void IncreasePlayCount(int count)
+    {
+        if(count >= 0) { 
+            playCount += count;
+        }
+        else
+        {
+            Console.WriteLine("Play count cannot be negative or zero.");
+        }
+    }
+
+    public void PrintTrackDetails()
+    {
+        Console.WriteLine("========== Track Details ==========");
+        Console.WriteLine($"ID          : {this.id}");
+        Console.WriteLine($"Title       : {this.title}");
+        Console.WriteLine($"Play Count  : {this.playCount}");
+        Console.WriteLine("===================================\n");
+    }
+}
